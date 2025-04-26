913399cfddc374f1da00ad787d7a4e85dadd6ad7
Stag51 <dev.saad.shabbir@gmail.com>
2025-04-27 03:25:47 +0500

    Added Generate Function to training loop

--- main.py
+++ main.py
@@ -329,4 +329,63 @@ class GPTModel(nn.Module):
         logits = self.out_ff(x)
         return logits
     
+#############           Training loop       ############
+
+# Generate Function
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+
+        # Getting the prediction
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        idx_next = torch.argmax(logits, dim= -1, keepdim= True)
+        idx = torch.cat((idx, idx_next), dim = 1)
+
+    return idx
+
+def generate(model, idx, max_new_tokens, context_size, temperature = 0.0, top_k = None, eos_id = None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(logits < min_val, torch.tensor(float('-inf')).to(logits.device), logits)
+
+        if temperature > 0.0:
+            logits = logits / temperature
+            probs = torch.softmax(logits, dim= -1)
+            idx_next = torch.multinomial(probs, num_samples=-1)
+
+        else: 
+            idx_next = torch.argmax(logits, dim = -1, keepdim= True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim = 1)
+
+
+    return idx
+
+def generate_print_sample(model, tokenizer, device, start_context, temperature, top_k, eos_id):
+    model.eval()
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_tokens_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate(
+            model= model,
+            idx = encoded,
+            max_new_tokens= 50,
+            context_size= context_size,
+            temperature= temperature,
+            top_k= top_k,
+            eos_id= eos_id
+        )
+
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", " "))
+    model.train()
     
