f9498a62b2eaead7b4077fb0b77fa1a8bfcb0e76
Stag51 <dev.saad.shabbir@gmail.com>
2025-04-27 03:49:42 +0500

    Added Evaluation Methodes

--- main.py
+++ main.py
@@ -415,3 +415,52 @@ def calc_loss_loader(data_loader, model, device, num_batches = None):
             break
 
     return total_loss / num_batches
+
+# Training and Evaluation Methode
+
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer, temperature, top_k, eos_id):
+    train_losses , val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        optimizer.zero_grad()
+
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:03d}): "
+                  f"Train loss {train_loss:.3f}, Val loss {val_loss:.3f}")
+        
+        # Printing the sample text
+        print("Example: ")
+        generate_print_sample(
+            model, 
+            tokenizer,
+            device,
+            start_context,
+            temperature,
+            top_k,
+            eos_id
+        )
+        print('-*-'*10)
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
